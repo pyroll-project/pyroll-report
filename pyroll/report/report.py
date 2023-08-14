0feac5d00ca14adca18842c697a12fbfee8c31ad
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-14 08:11:39 +0200

    Add Convenience Plot Functions  (#9)
    * impl report_to
    
    * impl show_report

--- pyroll/report/report.py
+++ pyroll/report/report.py
@@ -1,4 +1,7 @@
 import datetime
+import os
+import tempfile
+import webbrowser
 
 import jinja2
 from pathlib import Path
@@ -7,6 +10,8 @@ import platform
 from pyroll.core import PassSequence
 from pyroll.report.pluggy import plugin_manager
 
+from typing import Union, TextIO
+
 _env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(Path(__file__).parent, encoding="utf-8")
 )
@@ -14,9 +19,9 @@ _env = jinja2.Environment(
 
 def report(pass_sequence: PassSequence) -> str:
     """
-    Render an HTML report from the specified units list.
+    Render an HTML report from the specified pass sequence.
 
-    :param pass_sequence: list of units to take the data from
+    :param pass_sequence: PassSequence instance to take the data from
     :returns: generated HTML code as string
     """
 
@@ -29,3 +34,40 @@ def report(pass_sequence: PassSequence) -> str:
         platform=f"{platform.node()} ({platform.platform()}, {platform.python_implementation()} {platform.python_version()})",
         displays=displays,
     )
+
+
+def report_to(pass_sequence: PassSequence, file: Union[str, os.PathLike, TextIO]) -> int:
+    """
+    Render an HTML report from the specified pass sequence and save it directly to a file.
+
+    :param pass_sequence: PassSequence instance to take the data from
+    :param file: a str representing a path, a path-like object, or a file-like object with write permissions
+    to write the report to
+    :returns: the number of written bytes
+    """
+
+    result = report(pass_sequence)
+
+    try:
+        return file.write(result)
+
+    except AttributeError:
+        path = Path(file)
+        return path.write_text(result)
+
+
+def show_report(pass_sequence: PassSequence) -> Path:
+    """
+    Render an HTML report from the specified pass sequence, save it to a temporary file and open this in the webbrowser.
+
+    :param pass_sequence: PassSequence instance to take the data from
+    :returns: the path to the temporary file
+    """
+
+    result = report(pass_sequence)
+    with tempfile.NamedTemporaryFile("w", prefix="pyroll_report_", suffix=".html", delete=False) as file:
+        file.write(result)
+        path = Path(file.name)
+
+    webbrowser.open(path.as_uri())
+    return path
---
+++ tests/test_report_to.py
@@ -0,0 +1,54 @@
+import pyroll.core as pr
+import pytest
+
+from pyroll.report.report import report_to
+
+IN_PROFILE = pr.Profile.round(
+    diameter=4e-3,
+    strain=0,
+    flow_stress=100e6,
+)
+
+SEQUENCE = pr.PassSequence([
+    pr.RollPass(
+        roll=pr.Roll(
+            groove=pr.CircularOvalGroove(r1=1e-3, r2=5e-3, depth=1e-3),
+            nominal_radius=100e-3
+        ),
+        gap=1e-3,
+        velocity=1,
+    ),
+])
+
+SEQUENCE.solve(IN_PROFILE)
+
+
+def test_str(tmp_path):
+    f = tmp_path / "report.html"
+
+    report_to(SEQUENCE, str(f))
+
+    assert f.exists()
+
+
+def test_path(tmp_path):
+    f = tmp_path / "report.html"
+
+    report_to(SEQUENCE, f)
+
+    assert f.exists()
+
+
+def test_file_object(tmp_path):
+    f = tmp_path / "report.html"
+
+    report_to(SEQUENCE, f.open("w"))
+
+    assert f.exists()
+
+
+def test_file_object_bin(tmp_path):
+    f = tmp_path / "report.html"
+    with pytest.raises(TypeError):
+        # noinspection PyTypeChecker
+        report_to(SEQUENCE, f.open("wb"))
